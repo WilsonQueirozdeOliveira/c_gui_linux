c9014d1868085104b14f9a85292d9f7e554cf2ea
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-04-23 20:22:39 -0300

    create window

--- start.c
+++ start.c
@@ -4,8 +4,12 @@ int main (int argc, char **argv) {
 
 	gtk_init (&argc, &argv);
 
-	//gtk code comes here
+	GtkWidget *window;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	
+	gtk_widget_show_all(window);
+
 	gtk_main();
 
 	return 0;
